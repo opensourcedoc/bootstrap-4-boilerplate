da8a550c98753ea3471ac87a04ab0220a9e49ae2
Michael Chen <cwchen.tw@outlook.com>
2019-07-04 22:34:29 +0800

    Add json support

--- build/tasks/html/build.js
+++ build/tasks/html/build.js
@@ -1,13 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
 const isdev = require('isdev');
 
 const gulp = require('gulp');
+const data = require('gulp-data');
 const gulpif = require('gulp-if');
 
 const nunjucks = require('gulp-nunjucks-render');
 const htmlmin = require('gulp-htmlmin');
 
+const json = require('../../lib/json');
+
 module.exports = function () {
     return gulp.src('../src/*.html')
+        .pipe(data(json('../data/json/')))
         .pipe(nunjucks({
             path: ['../src']
         }))
